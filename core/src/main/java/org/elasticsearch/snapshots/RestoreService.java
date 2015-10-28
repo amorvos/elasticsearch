6f3f52e5c8777e88a9343e41958f9cd4bc9cf06b
Sebastian Utz <su@rtme.net>
2015-10-28 13:24:54 +0100

    do not overwrite existing templates during restore

--- core/src/main/java/org/elasticsearch/snapshots/RestoreService.java
+++ core/src/main/java/org/elasticsearch/snapshots/RestoreService.java
@@ -295,8 +295,8 @@ public class RestoreService extends AbstractComponent implements ClusterStateLis
                             }
                         }
 
-                        // restore templates which matches any restored index
-                        restoreTemplatesMatchingRestoredIndices(mdBuilder);
+                        // restore templates which matches any restored index (but do NOT overwrite existing templates)
+                        restoreTemplatesMatchingRestoredIndices(mdBuilder, currentState);
 
                         shards = shardsBuilder.build();
                         RestoreInProgress.Entry restoreEntry = new RestoreInProgress.Entry(snapshotId, RestoreInProgress.State.INIT, Collections.unmodifiableList(new ArrayList<>(renamedIndices.keySet())), shards);
@@ -307,8 +307,8 @@ public class RestoreService extends AbstractComponent implements ClusterStateLis
 
                     checkAliasNameConflicts(renamedIndices, aliases);
 
-                    // Restore global state if needed
-                    restoreGlobalStateIfRequested(mdBuilder);
+                    // Restore global state if needed (but do NOT overwrite existing templates)
+                    restoreGlobalStateIfRequested(mdBuilder, currentState);
 
                     if (completed(shards)) {
                         // We don't have any indices to restore - we are done
@@ -413,11 +413,12 @@ public class RestoreService extends AbstractComponent implements ClusterStateLis
                     return builder.settings(Settings.builder().put(settingsMap)).build();
                 }
 
-                private void restoreTemplatesMatchingRestoredIndices(MetaData.Builder mdBuilder) {
+                private void restoreTemplatesMatchingRestoredIndices(MetaData.Builder mdBuilder, ClusterState currentState) {
                     if (metaData.templates() != null) {
                         for (ObjectCursor<IndexTemplateMetaData> cursor : metaData.templates().values()) {
                             for (String index : filteredIndices) {
-                                if (Regex.simpleMatch(cursor.value.template(), index)) {
+                                if (currentState.metaData().templates().get(cursor.value.name()) == null
+                                        && Regex.simpleMatch(cursor.value.template(), index)) {
                                     mdBuilder.put(cursor.value);
                                     break;
                                 }
@@ -426,7 +427,7 @@ public class RestoreService extends AbstractComponent implements ClusterStateLis
                     }
                 }
 
-                private void restoreGlobalStateIfRequested(MetaData.Builder mdBuilder) {
+                private void restoreGlobalStateIfRequested(MetaData.Builder mdBuilder, ClusterState currentState) {
                     if (request.includeGlobalState()) {
                         if (metaData.persistentSettings() != null) {
                             boolean changed = false;
@@ -451,7 +452,9 @@ public class RestoreService extends AbstractComponent implements ClusterStateLis
                         if (metaData.templates() != null) {
                             // TODO: Should all existing templates be deleted first?
                             for (ObjectCursor<IndexTemplateMetaData> cursor : metaData.templates().values()) {
-                                mdBuilder.put(cursor.value);
+                                if (currentState.metaData().templates().get(cursor.value.name()) == null) {
+                                    mdBuilder.put(cursor.value);
+                                }
                             }
                         }
                         if (metaData.customs() != null) {
