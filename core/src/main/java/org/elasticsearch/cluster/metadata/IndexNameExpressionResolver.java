cb8e58be919388ca42bddbd4d9b574831dd0215b
Mathias Fussenegger <f.mathias@zignar.net>
2016-01-21 16:17:20 +0100

    change routing handling to work with empty strings and commas
    cherry-pick of 75c0ec92190e1670dba3fddddf9b6e040a74fff5 and
    03fc9b6e22ef3f542aaa0d62641c331426470003

--- core/src/main/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolver.java
+++ core/src/main/java/org/elasticsearch/cluster/metadata/IndexNameExpressionResolver.java
@@ -20,6 +20,7 @@
 package org.elasticsearch.cluster.metadata;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableSet;
 import org.elasticsearch.ElasticsearchParseException;
 import org.elasticsearch.action.IndicesRequest;
 import org.elasticsearch.action.support.IndicesOptions;
@@ -40,15 +41,7 @@ import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 import static com.google.common.collect.Maps.filterEntries;
@@ -305,6 +298,13 @@ public class IndexNameExpressionResolver extends AbstractComponent {
      * @return routing values grouped by concrete index
      */
     public Map<String, Set<String>> resolveSearchRouting(ClusterState state, @Nullable String routing, String... expressions) {
+        if (routing == null) {
+            return resolveSearchRouting(state, ImmutableSet.<String>of(), expressions);
+        }
+        return resolveSearchRouting(state, Strings.splitStringByCommaToSet(routing), expressions);
+    }
+
+    public Map<String, Set<String>> resolveSearchRouting(ClusterState state, Set<String> routing, String... expressions) {
         List<String> resolvedExpressions = expressions != null ? Arrays.asList(expressions) : Collections.<String>emptyList();
         Context context = new Context(state, IndicesOptions.lenientExpandOpen());
         for (ExpressionResolver expressionResolver : expressionResolvers) {
@@ -316,12 +316,8 @@ public class IndexNameExpressionResolver extends AbstractComponent {
         }
 
         Map<String, Set<String>> routings = null;
-        Set<String> paramRouting = null;
         // List of indices that don't require any routing
         Set<String> norouting = new HashSet<>();
-        if (routing != null) {
-            paramRouting = Strings.splitStringByCommaToSet(routing);
-        }
 
         for (String expression : resolvedExpressions) {
             AliasOrIndex aliasOrIndex = state.metaData().getAliasAndIndexLookup().get(expression);
@@ -342,8 +338,8 @@ public class IndexNameExpressionResolver extends AbstractComponent {
                                 routings.put(concreteIndex, r);
                             }
                             r.addAll(aliasMetaData.searchRoutingValues());
-                            if (paramRouting != null) {
-                                r.retainAll(paramRouting);
+                            if (!routing.isEmpty()) {
+                                r.retainAll(routing);
                             }
                             if (r.isEmpty()) {
                                 routings.remove(concreteIndex);
@@ -352,8 +348,8 @@ public class IndexNameExpressionResolver extends AbstractComponent {
                             // Non-routing alias
                             if (!norouting.contains(concreteIndex)) {
                                 norouting.add(concreteIndex);
-                                if (paramRouting != null) {
-                                    Set<String> r = new HashSet<>(paramRouting);
+                                if (!routing.isEmpty()) {
+                                    Set<String> r = new HashSet<>(routing);
                                     if (routings == null) {
                                         routings = newHashMap();
                                     }
@@ -371,8 +367,8 @@ public class IndexNameExpressionResolver extends AbstractComponent {
                 // Index
                 if (!norouting.contains(expression)) {
                     norouting.add(expression);
-                    if (paramRouting != null) {
-                        Set<String> r = new HashSet<>(paramRouting);
+                    if (!routing.isEmpty()) {
+                        Set<String> r = new HashSet<>(routing);
                         if (routings == null) {
                             routings = newHashMap();
                         }
@@ -395,13 +391,12 @@ public class IndexNameExpressionResolver extends AbstractComponent {
     /**
      * Sets the same routing for all indices
      */
-    private Map<String, Set<String>> resolveSearchRoutingAllIndices(MetaData metaData, String routing) {
-        if (routing != null) {
-            Set<String> r = Strings.splitStringByCommaToSet(routing);
+    private Map<String, Set<String>> resolveSearchRoutingAllIndices(MetaData metaData, Set<String> routing) {
+        if (!routing.isEmpty()) {
             Map<String, Set<String>> routings = newHashMap();
             String[] concreteIndices = metaData.concreteAllIndices();
             for (String index : concreteIndices) {
-                routings.put(index, r);
+                routings.put(index, routing);
             }
             return routings;
         }
