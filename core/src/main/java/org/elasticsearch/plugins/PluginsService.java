74da016227dfb8c38b64481de7efbfe588740b84
Mathias Fussenegger <f.mathias@zignar.net>
2016-04-21 17:28:57 +0200

    Skip bogus files in plugin directories

--- core/src/main/java/org/elasticsearch/plugins/PluginsService.java
+++ core/src/main/java/org/elasticsearch/plugins/PluginsService.java
@@ -287,6 +287,9 @@ public class PluginsService extends AbstractComponent {
                     logger.trace("--- skip hidden plugin file[{}]", plugin.toAbsolutePath());
                     continue;
                 }
+                if (!FileSystemUtils.isAccessibleDirectory(plugin, logger)) {
+                    continue;
+                }
                 PluginInfo info;
                 try {
                     info = PluginInfo.readFromProperties(plugin);
