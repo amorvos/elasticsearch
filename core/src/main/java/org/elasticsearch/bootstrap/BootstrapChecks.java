dc23941cbcc32225de1f22f3b68e67fd6bde866f
Sebastian Utz <su@rtme.net>
2017-03-31 13:57:35 +0200

    bootstrap checks: print `crate` instead of `elasticsearch` on errors

--- core/src/main/java/org/elasticsearch/bootstrap/BootstrapChecks.java
+++ core/src/main/java/org/elasticsearch/bootstrap/BootstrapChecks.java
@@ -272,7 +272,7 @@ final class BootstrapChecks {
         public final String errorMessage() {
             return String.format(
                 Locale.ROOT,
-                "max file descriptors [%d] for elasticsearch process is too low, increase to at least [%d]",
+                "max file descriptors [%d] for crate process is too low, increase to at least [%d]",
                 getMaxFileDescriptorCount(),
                 limit
             );
@@ -300,7 +300,7 @@ final class BootstrapChecks {
 
         @Override
         public String errorMessage() {
-            return "memory locking requested for elasticsearch process but memory is not locked";
+            return "memory locking requested for crate process but memory is not locked";
         }
 
         // visible for testing
