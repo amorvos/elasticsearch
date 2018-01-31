openeuler-mirror/poissonsearch-oss

Use port 0 InternalTestCluster nodes (#27859)
We currently have a complicated port assignment scheme to make sure that the nodes span off by the internal test cluster will be assigned fixed port ranges that will also not collide between clusters. The port ranges need to be fixed in advance so that the nodes will be able to find each other via `UnicastZenPing`.

This approach worked well for the last few years but we are now at a point that our testing has grown beyond it and we exceed the 5 reusable ranges per JVM. This means that nodes are not always assigned the first 5 ports in their range which causes cluster formation issues. On top of that, most of the clusters that are span up don't even rely on `UnicastZenPing` but rather `MockZenPings` that uses in memory maps for discovery (with the down side that they are not influenced by network disruption simulations).

This PR changes `InternalTestCluster` to use port 0 as a fixed assignment. This will allow the OS to manage ports and will ensure we don't have collisions. For tests that need to simulate network disruptions (and thus can't use `MockZenPings`), a new `UnicastHostProvider` is introduced that is based on the current state of the test cluster. Since that is only resolved at run time, it is aware of the port assignments of the OS.

Closes #27818
Closes #27762

test/framework/src/main/java/org/elasticsearch/test/discovery/TestZenDiscovery.java
@@ -19,15 +19,13 @@
 
 package org.elasticsearch.test.discovery;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
-
+import org.apache.lucene.util.SetOnce;
+import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.routing.allocation.AllocationService;
 import org.elasticsearch.cluster.service.ClusterApplier;
 import org.elasticsearch.cluster.service.MasterService;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.network.NetworkService;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
@@ -41,6 +39,14 @@ import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static org.elasticsearch.discovery.zen.UnicastZenPing.DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING;
+
 /**
  * A alternative zen discovery which allows using mocks for things like pings, as well as
  * giving access to internals.
@@ -53,9 +59,11 @@ public class TestZenDiscovery extends ZenDiscovery {
     /** A plugin which installs mock discovery and configures it to be used. */
     public static class TestPlugin extends Plugin implements DiscoveryPlugin {
         protected final Settings settings;
+        private final SetOnce<MockUncasedHostProvider> unicastHostProvider = new SetOnce<>();
         public TestPlugin(Settings settings) {
             this.settings = settings;
         }
+
         @Override
         public Map<String, Supplier<Discovery>> getDiscoveryTypes(ThreadPool threadPool, TransportService transportService,
                                                                   NamedWriteableRegistry namedWriteableRegistry,
@@ -63,8 +71,31 @@ public class TestZenDiscovery extends ZenDiscovery {
                                                                   ClusterSettings clusterSettings, UnicastHostsProvider hostsProvider,
                                                                   AllocationService allocationService) {
             return Collections.singletonMap("test-zen",
-                () -> new TestZenDiscovery(settings, threadPool, transportService, namedWriteableRegistry, masterService,
+                () -> new TestZenDiscovery(
+                    // we don't get the latest setting which were updated by the extra settings for the plugin. TODO: fix.
+                    Settings.builder().put(settings).putList(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey()).build(),
+                    threadPool, transportService, namedWriteableRegistry, masterService,
                     clusterApplier, clusterSettings, hostsProvider, allocationService));
+        }
+
+        @Override
+        public Map<String, Supplier<UnicastHostsProvider>> getZenHostsProviders(TransportService transportService,
+                                                                                NetworkService networkService) {
+            final Supplier<UnicastHostsProvider> supplier;
+            if (USE_MOCK_PINGS.get(settings)) {
+                // we have to return something in order for the unicast host provider setting to resolve to something. It will never be used
+                supplier = () -> () -> {
+                    throw new UnsupportedOperationException();
+                };
+            } else {
+                supplier = () -> {
+                    unicastHostProvider.set(
+                        new MockUncasedHostProvider(transportService::getLocalNode, ClusterName.CLUSTER_NAME_SETTING.get(settings))
+                    );
+                    return unicastHostProvider.get();
+                };
+            }
+            return Collections.singletonMap("test-zen", supplier);
         }
 
         @Override
@@ -74,7 +105,19 @@ public class TestZenDiscovery extends ZenDiscovery {
 
         @Override
         public Settings additionalSettings() {
-            return Settings.builder().put(DiscoveryModule.DISCOVERY_TYPE_SETTING.getKey(), "test-zen").build();
+            return Settings.builder()
+                .put(DiscoveryModule.DISCOVERY_TYPE_SETTING.getKey(), "test-zen")
+                .put(DiscoveryModule.DISCOVERY_HOSTS_PROVIDER_SETTING.getKey(), "test-zen")
+                .putList(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey())
+                .build();
+        }
+
+        @Override
+        public void close() throws IOException {
+            super.close();
+            if (unicastHostProvider.get() != null) {
+                unicastHostProvider.get().close();
+            }
         }
     }
 

amorvos/elasticsearch

test/framework/src/main/java/org/elasticsearch/test/discovery/TestZenDiscovery.java
@@ -19,15 +19,13 @@
 
 package org.elasticsearch.test.discovery;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Supplier;
-
+import org.apache.lucene.util.SetOnce;
+import org.elasticsearch.cluster.ClusterName;
 import org.elasticsearch.cluster.routing.allocation.AllocationService;
 import org.elasticsearch.cluster.service.ClusterApplier;
 import org.elasticsearch.cluster.service.MasterService;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
+import org.elasticsearch.common.network.NetworkService;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
@@ -41,6 +39,14 @@ import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static org.elasticsearch.discovery.zen.UnicastZenPing.DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING;
+
 /**
  * A alternative zen discovery which allows using mocks for things like pings, as well as
  * giving access to internals.
@@ -53,9 +59,11 @@ public class TestZenDiscovery extends ZenDiscovery {
     /** A plugin which installs mock discovery and configures it to be used. */
     public static class TestPlugin extends Plugin implements DiscoveryPlugin {
         protected final Settings settings;
+        private final SetOnce<MockUncasedHostProvider> unicastHostProvider = new SetOnce<>();
         public TestPlugin(Settings settings) {
             this.settings = settings;
         }
+
         @Override
         public Map<String, Supplier<Discovery>> getDiscoveryTypes(ThreadPool threadPool, TransportService transportService,
                                                                   NamedWriteableRegistry namedWriteableRegistry,
@@ -63,8 +71,31 @@ public class TestZenDiscovery extends ZenDiscovery {
                                                                   ClusterSettings clusterSettings, UnicastHostsProvider hostsProvider,
                                                                   AllocationService allocationService) {
             return Collections.singletonMap("test-zen",
-                () -> new TestZenDiscovery(settings, threadPool, transportService, namedWriteableRegistry, masterService,
+                () -> new TestZenDiscovery(
+                    // we don't get the latest setting which were updated by the extra settings for the plugin. TODO: fix.
+                    Settings.builder().put(settings).putArray(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey()).build(),
+                    threadPool, transportService, namedWriteableRegistry, masterService,
                     clusterApplier, clusterSettings, hostsProvider, allocationService));
+        }
+
+        @Override
+        public Map<String, Supplier<UnicastHostsProvider>> getZenHostsProviders(TransportService transportService,
+                                                                                NetworkService networkService) {
+            final Supplier<UnicastHostsProvider> supplier;
+            if (USE_MOCK_PINGS.get(settings)) {
+                // we have to return something in order for the unicast host provider setting to resolve to something. It will never be used
+                supplier = () -> () -> {
+                    throw new UnsupportedOperationException();
+                };
+            } else {
+                supplier = () -> {
+                    unicastHostProvider.set(
+                        new MockUncasedHostProvider(transportService::getLocalNode, ClusterName.CLUSTER_NAME_SETTING.get(settings))
+                    );
+                    return unicastHostProvider.get();
+                };
+            }
+            return Collections.singletonMap("test-zen", supplier);
         }
 
         @Override
@@ -74,7 +105,19 @@ public class TestZenDiscovery extends ZenDiscovery {
 
         @Override
         public Settings additionalSettings() {
-            return Settings.builder().put(DiscoveryModule.DISCOVERY_TYPE_SETTING.getKey(), "test-zen").build();
+            return Settings.builder()
+                .put(DiscoveryModule.DISCOVERY_TYPE_SETTING.getKey(), "test-zen")
+                .put(DiscoveryModule.DISCOVERY_HOSTS_PROVIDER_SETTING.getKey(), "test-zen")
+                .putArray(DISCOVERY_ZEN_PING_UNICAST_HOSTS_SETTING.getKey())
+                .build();
+        }
+
+        @Override
+        public void close() throws IOException {
+            super.close();
+            if (unicastHostProvider.get() != null) {
+                unicastHostProvider.get().close();
+            }
         }
     }
 